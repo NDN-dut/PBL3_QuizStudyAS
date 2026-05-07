247a728b9c5ba4534a390cddf729235f49245edd
simpboy2k6 <minhkhoa1611dn@gmail.com>
2026-05-07 10:48:02 +0700

    kkkkkk

---
+++ QuizStudyAS/Services/AdditionAlgrothim.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizStudyAS.Data;
+using QuizStudyAS.Models;
+
+namespace QuizStudyAS.Services
+{
+    public class AdditionAlgrothim : IAdditionAlgrothim
+    {
+        private readonly AppDbContext _context;
+        public AdditionAlgrothim(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<string> CreateUniqueLink()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string newLink = "";
+            bool isUnique = false;
+
+            while (!isUnique)
+            {
+                var codeArray = new char[8];
+                for (int i = 0; i < 8; i++)
+                {
+                    // Random.Shared.Next() lấy ngẫu nhiên 1 vị trí trong chuỗi chars
+                    codeArray[i] = chars[Random.Shared.Next(chars.Length)];
+                }
+                newLink = new string(codeArray);
+
+                bool LinkExists = await _context.Classrooms.AnyAsync(c => c.InviteCode == newLink);
+
+                if (!LinkExists)
+                {
+                    isUnique = true; // Mã duy nhất -> Dừng vòng lặp!
+                }
+            }
+
+            return newLink;
+        }
+        
+    }
+}
